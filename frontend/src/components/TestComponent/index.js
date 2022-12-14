28893ddf34c08d93432607e77cd81b6408f34588
Ardian-Kovanxhi <akovanxhi@gmail.com>
2022-12-14 00:53:35 -0500

    render test

---
+++ frontend/src/components/TestComponent/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TestButton() {
+    return (
+        <div>
+            <button>
+                I exist!!
+            </button>
+        </div>
+    )
+}
