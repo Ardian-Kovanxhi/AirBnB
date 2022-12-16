34fd4c2c6de5eb092446053aa2278a2fd1cd9824
Ardian-Kovanxhi <akovanxhi@gmail.com>
2022-12-16 11:09:55 -0500

    attempt at all spots, getting terminal spam when data renders

--- frontend/src/components/TestComponent/index.js
+++ frontend/src/components/TestComponent/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+// import * as spotActions from './store/spots'
+import { useDispatch, useSelector } from 'react-redux';
+import { getSpot, getSpots } from '../../store/spots';
 
 export default function TestButton() {
+    // getSpots()
+    const dispatch = useDispatch()
+    dispatch(getSpots())
+    // const sessionSpots = useSelector(state => state.spots.allSpots);
+
+
     return (
         <div>
-            <button>
-                I exist!!
-            </button>
+            <ul>
+                <li>
+                    {/* {useSelector(state => state.spots.allSpots)[1].name} */}
+                </li>
+            </ul>
         </div>
     )
 }
---
+++ frontend/src/store/spots.js
@@ -0,0 +1,56 @@
+import { csrfFetch } from "./csrf";
+
+const READ_SPOTS = 'spots/READ_SPOTS'
+const READ_SPOT = 'spot/READ_SPOT'
+
+const readSpots = (spots) => {
+    return {
+        type: READ_SPOTS,
+        spots
+    }
+}
+const readSpot = spot => {
+    return {
+        type: READ_SPOT,
+        spot
+    }
+}
+
+
+export const getSpots = () => async dispatch => {
+    const response = await csrfFetch('/api/spots')
+
+    if (response.ok) {
+        const spots = await response.json();
+        dispatch(readSpots(spots))
+        return spots
+    }
+}
+export const getSpot = () => async dispatch => {
+    const response = await csrfFetch('/api/spots/1')
+
+    if (response.ok) {
+        const spot = await response.json();
+        dispatch(readSpot(spot))
+        return spot
+    }
+}
+
+const initialState = { allSpots: {}, singleSpot: {} }
+
+export default function spotsReducer(state = initialState, action) {
+    switch (action.type) {
+        case READ_SPOTS: {
+            const newState = { allSpots: {}, singleSpot: {} }
+            action.spots.forEach(spot => newState.allSpots[spot.id] = spot);
+            return newState
+        }
+        case READ_SPOT: {
+            const newState = { ...state, singleSpot: {} }
+            newState.singleSpot = action.song
+            return newState
+        }
+        default:
+            return state;
+    }
+}
