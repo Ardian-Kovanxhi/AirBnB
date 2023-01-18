bdabc810a71831bc074ad10f7207fa24a700e443
Ardian-Kovanxhi <akovanxhi@gmail.com>
2023-01-17 22:52:09 -0500

    AllSpots set up with redirecting divs

--- frontend/src/components/Spots/AllSpots.js
+++ frontend/src/components/Spots/AllSpots.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 // import * as spotActions from './store/spots'
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { getSpot, getSpots } from '../../store/spots';
 import './spots.css'
 
 export default function HomeSpots() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    let history = useHistory();
+
     useEffect(() => {
         dispatch(getSpots())
     }, [])
@@ -14,17 +16,17 @@ export default function HomeSpots() {
     useEffect(() => {
         dispatch(getSpot())
     }, [])
+
     const sessionSpots = useSelector(state => state.spots.allSpots);
     const sessionSpot = useSelector(state => state.spots.singleSpot);
 
-    console.log(useParams())
     return (
         <div className='all-spots-home'>
             <h1>All Spots</h1>
             {Object.values(sessionSpots).map(spot => {
                 return (
                     <div
-                        onClick={() => alert(spot.id)}
+                        onClick={() => history.push(`/${spot.id}`)}
                         className='home-spots'
                     >
                         <div className='home-spots-info-div'>
